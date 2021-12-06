cd488ee3e873aac3618d0399b871652c15845288
Kyle Jackson <kyle@thepublicclouds.com>
2021-12-06 21:46:36 +1000

    Testing changes to formula

--- cvecli.rb
+++ cvecli.rb
@@ -1,11 +1,35 @@
 class Cvecli < Formula
-    desc "A CLI tool that allows CNAs to manage their organisation and submit CVEs"
+    desc "cvecli"
     homepage "https://github.com/wizedkyle/cvecli"
-    url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_arm64.tar.gz"
-    sha256 "1becbd9c81fc736ba741bc225a5a975f1836a5b410a95c776e7d76678f676451"
-    license "Apache-2.0"
-  
+    version "0.1.0"
+
+    if OS.mac? && Hardware::CPU.intel?
+        url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_arm64.tar.gz"
+        sha256 "1becbd9c81fc736ba741bc225a5a975f1836a5b410a95c776e7d76678f676451"
+    end
+
+    if OS.mac? && Hardware::CPU.arm?
+        url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_arm64.tar.gz"
+        sha256 "1becbd9c81fc736ba741bc225a5a975f1836a5b410a95c776e7d76678f676451"
+    end
+
+    if OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+        url ""
+        sha256 ""
+    end
+    
+    if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        url ""
+        sha256 ""
+    end
+
+    conflicts_with "cvecli"
+
     def install
-      bin.install 'cvecli'
+      bin.install "cvecli"
+    end
+
+    test do
+        system "#{bin}/cvecli --version"
     end
   end
