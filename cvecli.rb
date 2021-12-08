1c83ab1a32dacdc21a3032d44b7296b800403895
Kyle Jackson <kyle@thepublicclouds.com>
2021-12-08 19:56:51 +1000

    added darwin amd64 test

--- cvecli.rb
+++ cvecli.rb
@@ -3,8 +3,8 @@ class Cvecli < Formula
     homepage "https://github.com/wizedkyle/cvecli"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_arm64.tar.gz"
-        sha256 "1becbd9c81fc736ba741bc225a5a975f1836a5b410a95c776e7d76678f676451"
+        url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_amd64.tar.gz"
+        sha256 "9a23f12433923d8690eb458393ae5f94c9c2f1be09e06aaa96b8fd872bf239a2"
     end
 
     if OS.mac? && Hardware::CPU.arm?
