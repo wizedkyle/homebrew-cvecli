b327befcde8bcbdb05cdc2d7b5d8885401c926f2
Kyle Jackson <kyle@thepublicclouds.com>
2021-12-08 22:38:24 +1000

    Updated binary shas

--- Formula/cvecli.rb
+++ Formula/cvecli.rb
@@ -6,7 +6,7 @@ class Cvecli < Formula
     on_macos do
         if Hardware::CPU.intel?
             url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_amd64.tar.gz"
-            sha256 "9a23f12433923d8690eb458393ae5f94c9c2f1be09e06aaa96b8fd872bf239a2"
+            sha256 "df79cf37c5adaf9ea198dfa531fca649cefda31585d9dfdc4b06a27bc24e2f49"
 
             def install
                 bin.install "cvecli"
@@ -15,7 +15,7 @@ class Cvecli < Formula
 
         if Hardware::CPU.arm?
             url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_0.1.0_darwin_arm64.tar.gz"
-            sha256 "1becbd9c81fc736ba741bc225a5a975f1836a5b410a95c776e7d76678f676451"
+            sha256 "54b6ebd62a0f522847212d45ec6262e5b8a733e010f913a544cd494a4677250e"
 
             def install
                 bin.install "cvecli"
