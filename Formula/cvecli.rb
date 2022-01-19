80a410d0517e3ace0fedcbee5976248806f81f2c
Kyle Jackson <58360154+wizedkyle@users.noreply.github.com>
2022-01-19 22:29:52 +1000

    Updating cvecli formula to 1.5.0 (#30)

--- Formula/cvecli.rb
+++ Formula/cvecli.rb
@@ -1,12 +1,12 @@
 class Cvecli < Formula
   desc "cvecli"
   homepage "https://github.com/wizedkyle/cvecli"
-  version "1.4.0"
+  version "1.5.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/wizedkyle/cvecli/releases/download/1.4.0/cvecli_1.4.0_darwin_amd64.tar.gz"
-      sha256 "d302f938997ce582631ec1979f2e80e9abd011b0710518d0b3bd1f142be328f3"
+      url "https://github.com/wizedkyle/cvecli/releases/download/1.5.0/cvecli_1.5.0_darwin_amd64.tar.gz"
+      sha256 "817e825cfd36bf07b2c65e12d6174c19481ea814948b2bf6bde6b6c44c826233"
 
       def install
         bin.install "cvecli"
@@ -14,8 +14,8 @@ class Cvecli < Formula
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/wizedkyle/cvecli/releases/download/1.4.0/cvecli_1.4.0_darwin_arm64.tar.gz"
-      sha256 "e1a234abb16a20c85c6152274b604a0e2b53e333b3d22bf9c76d9015efdf6421"
+      url "https://github.com/wizedkyle/cvecli/releases/download/1.5.0/cvecli_1.5.0_darwin_arm64.tar.gz"
+      sha256 "23dbc80fb6a6b5760e763ed9a065f362f5a0dcb8634fb0165cd9a54834101aa7"
 
       def install
         bin.install "cvecli"
@@ -25,8 +25,8 @@ class Cvecli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/wizedkyle/cvecli/releases/download/1.4.0/cvecli_1.4.0_linux_amd64.tar.gz"
-      sha256 "e21369a1786b66364c6df0e0266bc05f34e834364d67a691b1c8c748e9c78b23"
+      url "https://github.com/wizedkyle/cvecli/releases/download/1.5.0/cvecli_1.5.0_linux_amd64.tar.gz"
+      sha256 "0aa6139e1724e8116900f1b09565ed2daf4e8ea8a27793286b2383cac797b024"
 
       def install
         bin.install "cvecli"
@@ -34,8 +34,8 @@ class Cvecli < Formula
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_1.4.0_linux_amd64.tar.gz"
-      sha256 "da590123928384ba92d539b972020c3cb04effb3a6e3930c98b19a8bf2d22788"
+      url "https://github.com/wizedkyle/cvecli/releases/download/0.1.0/cvecli_1.5.0_linux_amd64.tar.gz"
+      sha256 "8bd9729329447a8ee6dbb1c98b3ee0d39f8054e9c2dcffd1ddd2dcc6583e496c"
 
       def install
         bin.install "cvecli"
